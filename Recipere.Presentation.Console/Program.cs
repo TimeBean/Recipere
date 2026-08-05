c1b90004c1320b727ccfe94767b4f3b1076f9672
TimeBean <alexanderklush8@gmail.com>
2026-08-05 15:38:32 +0300

    feat(console): add sample console client

---
+++ Recipere.Presentation.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Recipere.Application.Get;
+using Recipere.Core.Repository;
+using Recipere.Infrastructure.Repository;
+
+var services = new ServiceCollection();
+
+services.AddLogging();
+services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(GetRequest).Assembly));
+services.Configure<YtDlpOptions>(_ => { });
+services.AddSingleton<IContentRepository, YtDlpContentRepository>();
+
+await using var provider = services.BuildServiceProvider();
+
+var mediator = provider.GetRequiredService<ISender>();
+
+var content = await mediator.Send(new GetRequest("https://www.youtube.com/watch?v=O6B7X6R0_Sg"));
+
+Console.WriteLine(content.Title + " " + content.Channel.Name);
